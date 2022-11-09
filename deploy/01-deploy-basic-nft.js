afdb1483ed07b75c8075006dc08f863fcb93b1a1
Maruko-coding <markojau@gmail.com>
2022-11-09 13:48:00 -0300

    BasicNFT

---
+++ deploy/01-deploy-basic-nft.js
@@ -0,0 +1,26 @@
+const { network, getNamedAccounts } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async function({ getNamedAccounts, deployments }) {
+  const { deploy, log } = deployments
+  const { deployer } = await getNamedAccounts()
+
+  console.log("----------------------------")
+  const args = []
+  const basicNft = await deploy("BasicNFT", {
+    from: deployer,
+    args: args,
+    log: true,
+    waitConfirmations: network.config.blockConfirmations || 1
+  })
+
+  if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+    console.log("Veryfing...")
+    await verify(basicNft.address, args)
+  }
+
+  console.log("----------------------------")
+}
+
+module.exports.tags = ["all", "BasicNFT"]
